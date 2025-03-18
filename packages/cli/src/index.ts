6ed1e677ed5124e9d0957217b6082c51025f803c
devbamboo15 <devbamboo815@gmail.com>
2025-03-18 18:10:26 -0400

    bugfix: event names for Governo

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -14,7 +14,7 @@ import { setContext } from "./context";
 import { parseKeypair } from "./utils";
 
 program
-  .version("0.4.0")
+  .version("0.4.2")
   .option("-k, --keypair <path>", "wallet keypair", parseKeypair)
   .option("-u, --url <string>", "RPC monk or url", "devnet")
   .option("-p, --priority-level <string...>", "priority fee level", "High")
