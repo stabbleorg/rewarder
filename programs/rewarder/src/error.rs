2961082a02581934ffdcc5cf1c91be2130a58d95
stabble-devops <keisuke@stabble.org>
2025-03-27 12:16:03 -0400

    feat: reclaim rent fees for miners

--- programs/rewarder/src/error.rs
+++ programs/rewarder/src/error.rs
@@ -3,5 +3,8 @@ use anchor_lang::prelude::*;
 #[error_code]
 pub enum RewarderError {
     #[msg("The specified mint is not supported")]
-    NotSupportedMint,
+    UnsupportedMint,
+
+    #[msg("No rewards are currently available for claiming")]
+    NoClaimableRewards,
 }
