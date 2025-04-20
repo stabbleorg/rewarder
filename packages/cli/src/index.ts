1605c0f1b352f4afa0da8f423ef46d0cb7519915
stabble-devops <keisuke@stabble.org>
2025-04-20 08:36:07 -0400

    chore: bump Anchor version to 0.31.1

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -14,7 +14,7 @@ import { setContext } from "./context";
 import { parseKeypair } from "./utils";
 
 program
-  .version("0.9.4")
+  .version("0.10.0")
   .option("-k, --keypair <path>", "wallet keypair", parseKeypair)
   .option("-u, --url <string>", "RPC monk or url", "devnet")
   .option("-a, --alt-keys <string...>", "Address Lookup Table keys")
